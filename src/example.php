43249d83983a05188ecec1e81846d2e3a009ea60
rendermani <ml@webrender.de>
2019-01-31 11:53:40 +0100

    Fixed autoload location

--- src/example.php
+++ src/example.php
@@ -1,5 +1,5 @@
 <?php
-require_once("vendor/autoload.php");
+require_once("../vendor/autoload.php");
 use ascio\v3 as v3;
 use ascio\v2 as v2;
 use ascio\dns as dns; 
@@ -12,6 +12,7 @@ use ascio\v2\MessageType;
 /**
  * Please place the config.json in the root of your project and edit it.
  */
+
 
 $client = lib\Ascio::getClientV2("testing");
 $pollMessage = new PollMessage(0,MessageType::Message_to_Partner);
