735f4adfc614fb2d919f3ced20c8311282d81ceb
rendermani <ml@webrender.de>
2019-02-04 19:31:31 +0100

    debug

--- src/lib/PostUpdate.php
+++ src/lib/PostUpdate.php
@@ -10,12 +10,14 @@ class Update
     public static function postUpdate(Event $event)
     {
         $composer = $event->getComposer();
+        file_put_contents("test.txt","avc");
         var_dump($composer);
         // do stuff
     }
     public static function postPackageInstall(PackageEvent $event)
     {
         $installedPackage = $event->getOperation()->getPackage();
+        file_put_contents("test2.txt","avc");
         var_dump($installedPackage);
         // do stuff
     }
