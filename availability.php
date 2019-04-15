34e79b48e3bb4a55b9b228d2623aae03fa63def6
rendermani <ml@webrender.de>
2019-04-15 13:47:55 +0200

    removed includes

--- availability.php
+++ availability.php
@@ -2,23 +2,6 @@
 require_once(__DIR__."/vendor/autoload.php");
 header('Content-Type: application/json');
 
-// these classes are only needed for development. 
-// when using with composer, only the autoload.php is needed. 
-
-$d = dir(__DIR__."/src/v2/service");
-while (false !== ($entry = $d->read())) {
-    if(strpos($entry,".")!==0) {
-        require_once(__DIR__."/src/v2/service/".$entry);
-    }    
-}
-require_once(__DIR__."/src/lib/Ascio.php");
-require_once(__DIR__."/src/lib/v2/Domain.php");
-require_once(__DIR__."/src/lib/v2/Order.php");
-require_once(__DIR__."/src/lib/Tools.php");
-require_once(__DIR__."/src/lib/Actions.php");
-
-// end development includes
-
 use ascio\lib\Ascio;
 use ascio\lib\AscioEnvironment;
 use ascio\lib\v2\Order;
@@ -32,8 +15,8 @@ use ascio\v2\AvailabilityInfo;
 use ascio\v2\QualityType;
 
 $config = "/Users/ml/ownCloud/Keys/ascio/ascio.json";
-Ascio::setConfigFile($config);
 Ascio::setEnvironment(AscioEnvironment::Testing);
+Ascio::setConfigFile($config);
 
 try {
     $client = Ascio::getClientV2();
